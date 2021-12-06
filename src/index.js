cb3266059acc2e55c1847378b0e80eabdfeaa8df
lenny-g <leanne_gallag@live.co.uk>
2021-12-06 18:46:08 +0000

    set up server

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+// const routes = require("./routes");
+
+const app = express();
+
+const PORT = 3000;
+
+// express middlewares
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+// app.use(routes);
+
+app.listen(PORT, () =>
+  console.log(`server running on https://localhost:${PORT}`)
+);
